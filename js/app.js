1a1779d81099e2622e2624062d096a0366671d4b
Ryan Meyers <ryan.d.meyers@gmail.com>
2017-11-15 22:09:25 -0500

    update to show multiple tags, format better

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@
 // Make a list of all of the cafes and locations I'd like to include
 
 var locations = [
-    {'name' : 'Cafe Plenty',
-     'location' : {lat: 43.648948, lng: -79.378311}},
-    {'name' : 'Cafe Victoria',
-     'location' : {lat: 43.649694, lng:  -79.376775}},
+    {title : 'Cafe Plenty',
+     location : {lat: 43.64948, lng: -79.378311}},
+    {title : 'Cafe Victoria',
+     location : {lat: 43.649694, lng:  -79.376775}},
 ];
 
 var marker, infowindow, map;
+var markers = [];
 
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
@@ -17,18 +18,128 @@ function initMap() {
         zoom: 14,
     });
 
-    for (i=0; i<locations.length; i++){
-        marker = new google.maps.Marker({
-            position: locations[i].location,
-            map: map,
-            title: locations[i].name
-        });
-        infowindow = new google.maps.InfoWindow({
-            content: "This is where the content would go!"
-        });
-        marker.addListener('click', function() {
-            infowindow.open(map, marker);
-        });
+    var largeInfowindow = new google.maps.InfoWindow();
 
+    // Style the markers a bit. This will be our listing marker icon.
+    var defaultIcon = makeMarkerIcon('0091ff');
+
+    // Create a "highlighted location" marker color for when the user
+    // mouses over the marker.
+    var highlightedIcon = makeMarkerIcon('FFFF24');
+
+    // The following group uses the location array to create an array of markers on initialize.
+    for (var i = 0; i < locations.length; i++) {
+      // Get the position from the location array.
+      var position = locations[i].location;
+      var title = locations[i].title;
+      // Create a marker per location, and put into markers array.
+      var marker = new google.maps.Marker({
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        icon: defaultIcon,
+        id: i
+      });
+      // Push the marker to our array of markers.
+      markers.push(marker);
+      marker.setMap(map);
+
+      // Create an onclick event to open the large infowindow at each marker.
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow);
+      });
+      // Two event listeners - one for mouseover, one for mouseout,
+      // to change the colors back and forth.
+      marker.addListener('mouseover', function() {
+        this.setIcon(highlightedIcon);
+      });
+      marker.addListener('mouseout', function() {
+        this.setIcon(defaultIcon);
+      });
     }
+
+    document.getElementById('show-listings').addEventListener('click', showListings);
+    document.getElementById('hide-listings').addEventListener('click', hideListings);
 }
+
+// #############################################################################################
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+// Check to make sure the infowindow is not already opened on this marker.
+if (infowindow.marker != marker) {
+  // Clear the infowindow content to give the streetview time to load.
+  infowindow.setContent('');
+  infowindow.marker = marker;
+  // Make sure the marker property is cleared if the infowindow is closed.
+  infowindow.addListener('closeclick', function() {
+    infowindow.marker = null;
+  });
+  var streetViewService = new google.maps.StreetViewService();
+  var radius = 50;
+  // In case the status is OK, which means the pano was found, compute the
+  // position of the streetview image, then calculate the heading, then get a
+  // panorama from that and set the options
+  function getStreetView(data, status) {
+    if (status == google.maps.StreetViewStatus.OK) {
+      var nearStreetViewLocation = data.location.latLng;
+      var heading = google.maps.geometry.spherical.computeHeading(
+        nearStreetViewLocation, marker.position);
+        infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+        var panoramaOptions = {
+          position: nearStreetViewLocation,
+          pov: {
+            heading: heading,
+            pitch: 30
+          }
+        };
+      var panorama = new google.maps.StreetViewPanorama(
+        document.getElementById('pano'), panoramaOptions);
+    } else {
+      infowindow.setContent('<div>' + marker.title + '</div>' +
+        '<div>No Street View Found</div>');
+    }
+  }
+  // Use streetview service to get the closest streetview image within
+  // 50 meters of the markers position
+  streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+  // Open the infowindow on the correct marker.
+  infowindow.open(map, marker);
+}
+}
+
+// This function will loop through the markers array and display them all.
+function showListings() {
+var bounds = new google.maps.LatLngBounds();
+// Extend the boundaries of the map for each marker and display the marker
+for (var i = 0; i < markers.length; i++) {
+  markers[i].setMap(map);
+  bounds.extend(markers[i].position);
+}
+}
+
+// This function will loop through the listings and hide them all.
+function hideListings() {
+for (var i = 0; i < markers.length; i++) {
+  markers[i].setMap(null);
+}
+}
+
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
+function makeMarkerIcon(markerColor) {
+var markerImage = new google.maps.MarkerImage(
+  'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+  '|40|_|%E2%80%A2',
+  new google.maps.Size(21, 34),
+  new google.maps.Point(0, 0),
+  new google.maps.Point(10, 34),
+  new google.maps.Size(21,34));
+return markerImage;
+}
+
+
+
