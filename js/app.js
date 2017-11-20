cea3369ad457e75528a04048951ca0cd77935b27
Ryan Meyers <ryan.d.meyers@gmail.com>
2017-11-19 19:05:44 -0500

    Substantial update to meet most specifications

--- js/app.js
+++ js/app.js
@@ -1,145 +1,133 @@
 
-// Make a list of all of the cafes and locations I'd like to include
-
+// This is the list of all the cafes in Toronto that I want to include in this tool
 var locations = [
     {title : 'Cafe Plenty',
      location : {lat: 43.64948, lng: -79.378311}},
     {title : 'Cafe Victoria',
      location : {lat: 43.649694, lng:  -79.376775}},
+    {title : 'Infuse Cafe',
+     location : {lat: 43.658069, lng:  -79.382087}},
+    {title : 'Timothy\'s',
+     location : {lat: 43.656020, lng:  -79.382773}},
+    {title : 'Balzac\'s',
+     location : {lat: 43.657712, lng:  -79.379362}},
 ];
 
-var marker, infowindow, map;
-var markers = [];
+// Initiate the map global variable
+var map;
 
-function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 43.648181, lng: -79.378974},
-        zoom: 14,
-    });
+// View Model function
+function ViewModel() {
+  var self = this;
 
-    var largeInfowindow = new google.maps.InfoWindow();
+  // Create the variables for locationList and the searchTerm
+  this.locationList = ko.observableArray([]);
+  this.searchTerm = ko.observable('');
 
-    // Style the markers a bit. This will be our listing marker icon.
-    var defaultIcon = makeMarkerIcon('0091ff');
-
-    // Create a "highlighted location" marker color for when the user
-    // mouses over the marker.
-    var highlightedIcon = makeMarkerIcon('FFFF24');
-
-    // The following group uses the location array to create an array of markers on initialize.
-    for (var i = 0; i < locations.length; i++) {
-      // Get the position from the location array.
-      var position = locations[i].location;
-      var title = locations[i].title;
-      // Create a marker per location, and put into markers array.
-      var marker = new google.maps.Marker({
-        position: position,
-        title: title,
-        animation: google.maps.Animation.DROP,
-        icon: defaultIcon,
-        id: i
-      });
-      // Push the marker to our array of markers.
-      markers.push(marker);
-      marker.setMap(map);
-
-      // Create an onclick event to open the large infowindow at each marker.
-      marker.addListener('click', function() {
-        populateInfoWindow(this, largeInfowindow);
-      });
-      // Two event listeners - one for mouseover, one for mouseout,
-      // to change the colors back and forth.
-      marker.addListener('mouseover', function() {
-        this.setIcon(highlightedIcon);
-      });
-      marker.addListener('mouseout', function() {
-        this.setIcon(defaultIcon);
-      });
-    }
-
-    document.getElementById('show-listings').addEventListener('click', showListings);
-    document.getElementById('hide-listings').addEventListener('click', hideListings);
-}
-
-// #############################################################################################
-
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
-function populateInfoWindow(marker, infowindow) {
-// Check to make sure the infowindow is not already opened on this marker.
-if (infowindow.marker != marker) {
-  // Clear the infowindow content to give the streetview time to load.
-  infowindow.setContent('');
-  infowindow.marker = marker;
-  // Make sure the marker property is cleared if the infowindow is closed.
-  infowindow.addListener('closeclick', function() {
-    infowindow.marker = null;
+  // Create the map using the coords for the center of Toronto
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 43.648181, lng: -79.378974},
+      zoom: 14,
   });
-  var streetViewService = new google.maps.StreetViewService();
-  var radius = 50;
-  // In case the status is OK, which means the pano was found, compute the
-  // position of the streetview image, then calculate the heading, then get a
-  // panorama from that and set the options
-  function getStreetView(data, status) {
-    if (status == google.maps.StreetViewStatus.OK) {
-      var nearStreetViewLocation = data.location.latLng;
-      var heading = google.maps.geometry.spherical.computeHeading(
-        nearStreetViewLocation, marker.position);
-        infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-        var panoramaOptions = {
-          position: nearStreetViewLocation,
-          pov: {
-            heading: heading,
-            pitch: 30
-          }
-        };
-      var panorama = new google.maps.StreetViewPanorama(
-        document.getElementById('pano'), panoramaOptions);
+
+  // Create the markers and add them all to the location list
+  // This runs the "Marker" function, below
+  locations.forEach(function(location) {
+    self.locationList.push(new Marker(location));
+  });
+
+  // This modifies the filtered list based on whether or not the search term is found in the
+  // cafe names.
+  this.filteredList = ko.computed(function() {
+    // Make the search case insensitive
+    var search = self.searchTerm().toLowerCase();
+    // If no search being performed, show all locations.
+    if (!search) {
+      self.locationList().forEach(function(eachLocation){
+        eachLocation.visible(true);
+      });
+      return self.locationList();
     } else {
-      infowindow.setContent('<div>' + marker.title + '</div>' +
-        '<div>No Street View Found</div>');
+      // If there is a search being performed, use the Knockout arrayFilter function
+      var updatedList = ko.utils.arrayFilter(self.locationList(), function(eachLocation) {
+        var lowercaseName = eachLocation.title.toLowerCase();
+        // If the search term is found in the cafe name, result will be true, otherwise false.
+        var result = (lowercaseName.search(search) >= 0);
+        eachLocation.visible(result);
+        return result;
+      });
+      return updatedList;
     }
-  }
-  // Use streetview service to get the closest streetview image within
-  // 50 meters of the markers position
-  streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-  // Open the infowindow on the correct marker.
-  infowindow.open(map, marker);
-}
+  });
 }
 
-// This function will loop through the markers array and display them all.
-function showListings() {
-var bounds = new google.maps.LatLngBounds();
-// Extend the boundaries of the map for each marker and display the marker
-for (var i = 0; i < markers.length; i++) {
-  markers[i].setMap(map);
-  bounds.extend(markers[i].position);
-}
+// This function creates the marker for each location, as well as the content for the marker.
+function Marker(location) {
+  var self = this;
+
+  // Initiate the variables
+  this.address1 = '';
+  this.address2 = '';
+  this.checkins = 0;
+  this.position = location.location;
+  this.title = location.title;
+  this.visible = ko.observable(true);
+  var lat = location.location.lat;
+  var lon = location.location.lng;
+
+  var clientId = 'WJDD5S3RCIOTUKI4NLSKQYMW0ODMTC3X2IPMHDCIRJOHUQZQ';
+  var clientSecret = 'V1B3EVZJLF1HQF5NH5KGQR42EHGO244S0DOOQTFGJUGJQEOT';
+  var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ lat + ',' + lon + '&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20160118' + '&query=' + this.title;
+
+  // Run the AJAX call using JQuery to get the Four Square results.
+  $.getJSON(foursquareURL).done(function(data) {
+    var results = data.response.venues[0];
+    self.address1 = results.location.formattedAddress[0];
+    self.address2 = results.location.formattedAddress[1];
+    self.checkins = results.stats.checkinsCount;
+  }).fail(function() {
+    alert("FourSquare API call error. Try again.");
+  });
+
+  this.infoWindow = new google.maps.InfoWindow({content: self.windowContent});
+
+  // Create the marker and add to the map
+  this.marker = new google.maps.Marker({
+    position: this.position,
+    title: this.title,
+    animation: google.maps.Animation.DROP,
+    map: map
+  });
+
+  // This is where the search comes into play. If the visible state is false, hide the marker.
+  this.showMarker = ko.computed(function() {
+    if(this.visible() === true) {
+      this.marker.setMap(map);
+    } else {
+      this.marker.setMap(null);
+    }
+    return true;
+  }, this);
+
+  // Add a listener to the marker so that a click event shows the info window.
+  this.marker.addListener('click', function(){
+    self.windowContent = '<div><strong>' + self.title + '</strong></div>'
+      + '<div><i>Address:</i> ' + self.address1 + '</div>'
+      + '<div>' + self.address2 + '</div>'
+      + '<div><i>FourSquare Checkins: </i>' + self.checkins + '</div>';
+    self.infoWindow.setContent(self.windowContent);
+    self.infoWindow.open(map, this);
+  });
 }
 
-// This function will loop through the listings and hide them all.
-function hideListings() {
-for (var i = 0; i < markers.length; i++) {
-  markers[i].setMap(null);
-}
-}
+// Function to be run on page load which links the model to the view.
+var initApp = function() {
+  ko.applyBindings(new ViewModel());
+};
 
-// This function takes in a COLOR, and then creates a new marker
-// icon of that color. The icon will be 21 px wide by 34 high, have an origin
-// of 0, 0 and be anchored at 10, 34).
-function makeMarkerIcon(markerColor) {
-var markerImage = new google.maps.MarkerImage(
-  'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-  '|40|_|%E2%80%A2',
-  new google.maps.Size(21, 34),
-  new google.maps.Point(0, 0),
-  new google.maps.Point(10, 34),
-  new google.maps.Size(21,34));
-return markerImage;
-}
+
+
+
 
 
 
